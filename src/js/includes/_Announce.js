83e29c748ca3955ab2d2bc9a1d56a9a6a4b3d80f
EmilEinarsen <47479113+EmilEinarsen@users.noreply.github.com>
2020-09-04 08:49:59 +0200

    Updated some text in the article and thumbnail

--- src/js/includes/_Announce.js
+++ src/js/includes/_Announce.js
@@ -1,12 +1,17 @@
 function Announce() {
 	this.events = async() => {
-		let [intensive, leader] = tools.structureApprouchingEvents(await server.getEvents())
+		let [intensive, leader, calm] = tools.structureApprouchingEvents(await server.getEvents())
 		let events = `Nästa kurstillfällen: `
 
 		if(intensive) {
 			page.addInnerOf('#intensiveEventDates', `Nästa kurstillfälle är den ${intensive}.<br><br>`)
 			events = `${events} Intensiv Jägarexamen, den ${intensive}.`
 		} else page.hideMe('#intensiveEventDates')
+		
+		if(calm) {
+			page.addInnerOf('#calmEventDates', `Nästa kurstillfälle är den ${calm}.<br><br>`)
+			events = `${events} Lugn Jägarexamen, den ${calm}.`
+		} else page.hideMe('#calmEventDates')
 
 		if(leader) {
 			page.addInnerOf('#leadershipEventDates', `Nästa kurstillfälle är den ${leader}.<br><br>`)
